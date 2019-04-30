f18545992be718cb4029a1a8addca28495c38533
Matthew Fan <mfan@umd.edu>
2019-04-30 17:47:04 -0400

    increase hind leg propulsion step, add initial bend pose w/ IK

--- arduino_squirrel_v2/arduino_squirrel_v2.ino
+++ arduino_squirrel_v2/arduino_squirrel_v2.ino
@@ -17,6 +17,7 @@
 
 Servo servos[6];
 int pins[6] = {3,5,6,9,10,11};
+int target_angle[6];
 
 void setAngle(int motor, int angle){
   // writes the angle
@@ -27,6 +28,7 @@ void setAngle(int motor, int angle){
   } else if (motor % 2 == RIGHT){ // Right side motors are odd
     servos[motor].write(MAX_ANGLE - angle);
   }
+  target_angle[motor] = angle;
 }
 
 void setFrontLegs(int angle){
@@ -43,12 +45,19 @@ void setHindLegs(int angle_upper, int angle_lower){
 
 int retrieveAngle(int motor) {
   // servo.read(), but with angle adjustments accounted for
-  return motor % 2 == LEFT ? servos[motor].read() - MIN_ANGLE : MAX_ANGLE - servos[motor].read();
+  //  return motor % 2 == LEFT ? servos[motor].read() - MIN_ANGLE : MAX_ANGLE - servos[motor].read();
+  return target_angle[motor];
 }
 
-void bend(int height){
+void bend(){
+  // Take front leg positions, and calculate back leg joint angles to lie 'flat' through inverse kinematics
+  // Bend position is additionally constrained so that shoulder and foot are laterally coincident (i.e. share a vertical axis)
   int h_left = LEN_F * sin(float(servos[0].read() * PI) / 180);
   int h_right  =  LEN_F * sin(float(servos[1].read() * PI) / 180);
+  setAngle(2, 180 * acos((LEN_BU*LEN_BU + h_left*h_left - LEN_BL*LEN_BL) / (2*LEN_BU*h_left)) / PI);
+  setAngle(3, 180 * acos((LEN_BU*LEN_BU + h_right*h_right - LEN_BL*LEN_BL) / (2*LEN_BU*h_right)) / PI);
+  setAngle(4, 180 * acos((h_left*h_left - LEN_BU*LEN_BU - LEN_BL*LEN_BL) / (2*LEN_BU*LEN_BL)) / PI);
+  setAngle(5, 180 * acos((h_right*h_right - LEN_BU*LEN_BU - LEN_BL*LEN_BL) / (2*LEN_BU*LEN_BL)) / PI);
 }
 
 void setAngleAll(int angle){
@@ -61,7 +70,10 @@ void setup() {
   for (int i = 0; i<6; i++){ //initialize servos
     servos[i].attach(pins[i]);
   }
-  setAngleAll(0);
+//  setAngleAll(0);
+  setFrontLegs(70);
+  bend();
+  delay(1000);
   //@TODO: initialize sensor
 }
 
@@ -74,6 +86,6 @@ void loop() {
   delay(150);
   setFrontLegs(30);
   delay(50);
-  setHindLegs(110,0);
-  delay(300);
+  setHindLegs(130,0);
+  delay(350);
 }
